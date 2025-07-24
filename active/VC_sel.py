95d742bd21ac7a753707d54300fb706f1d48a78d
lly00412 <315885469@qq.com>
2025-07-23 21:29:42 -0400

    update bg mask

--- active/VC_sel.py
+++ active/VC_sel.py
@@ -84,7 +84,7 @@ class VCSelector(torch.nn.Module):
             ###############################
             # bg_mask_per_channel = (pred_img == background.view(3, 1, 1))
             # bg_mask = bg_mask_per_channel.all(dim=0)
-            bg_mask = (depth.squeeze() > 0)
+            bg_mask = ~(depth.squeeze() > 0)
             _, h,w = pred_img.shape
 
             numels = float(self.n_vcam) - nv_mask.sum(0)
