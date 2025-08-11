6c163776c6639169d20a0ae3947b9c3a442d606a
lly00412 <315885469@qq.com>
2025-08-11 17:12:37 -0400

    update scripts

--- active/schema.py
+++ active/schema.py
@@ -107,15 +107,73 @@ class VNSeqMInplace(BaseSchema):
                 num_views_left -= M
 
 
+class VNSeqMReplace(BaseSchema):
+    """
+    Add 1 image at a time
+    """
+
+    def __init__(self, dataset_size: int, scene, M: int = 1, num_init_views: int = 1, interval_epochs=100,
+                 **kwargs):
+        """
+        N: int total views to select
+        M: # views to select each time
+        """
+        super().__init__()
+        self.init_views = [0]
+
+        self.load_its = {}
+        num_init_views_needed = num_init_views - len(self.init_views)
+
+        # NOTE: following the default implementation of scene.
+        # result of getTrainCameras are shuffled in-place, thus we have to rely on train_idxs to index the views
+        all_cams = scene.train_cameras[1.0]
+        candidate_views = [i for i in range(len(all_cams)) if i not in self.init_views]
+        train_cams = [all_cams[i] for i in self.init_views]
+        candidate_cams = [all_cams[i] for i in candidate_views]
+
+        selected_idxs = []
+
+        for _ in range(num_init_views_needed):
+            trainT = torch.stack([i.camera_center.cpu() for i in train_cams])
+            candidateT = torch.stack([i.camera_center.cpu() for i in candidate_cams])
+
+            dist_mat = torch.cdist(candidateT, trainT)
+            candidate_dist = reduce(dist_mat, "c t -> c", "min")
+
+            selected_idx = candidate_dist.argmax().item()
+            selected_idxs.append(candidate_views.pop(selected_idx))
+
+            # Put selected cam into training cam
+            train_cams.append(candidate_cams.pop(selected_idx))
+
+        self.init_views.extend(selected_idxs)
+
+        cur_dataset_size = len(self.init_views)
+        it_base = cur_dataset_size * interval_epochs
+        num_views_left = dataset_size - len(self.init_views)
+
+        if num_views_left > 0:
+            assert num_views_left % M == 0, "cannot split M evenly to the rest views"
+
+            while num_views_left > 0:
+                self.load_its[it_base] = M
+
+                cur_dataset_size = M + len(self.init_views)
+                it_base += cur_dataset_size * interval_epochs
+                num_views_left -= M
+
+
 V20Seq1Inplace = partial(VNSeqMInplace, N=20, M=1, num_init_views=4,interval_epochs=100)
 V10Seq1Inplace = partial(VNSeqMInplace, N=10, M=1, num_init_views=2)
 V20Seq4Inplace = partial(VNSeqMInplace, N=20, M=4, num_init_views=4, interval_epochs=300)
 
+AllSeq1Replace = partial(VNSeqMReplace, M=1, num_init_views=4, interval_epochs=100)
 
 
 schema_dict: Dict[str, BaseSchema] = {'all': All, "debug": V20Seq1Debug,
                                       "v20seq1_inplace": V20Seq1Inplace, "v10seq1_inplace": V10Seq1Inplace,
                                       "v20seq4_inplace": V20Seq4Inplace,
+                                      "allseq1_inplace":AllSeq1Replace,
                                       }
 
 override_test_idxs_dict: Dict[str, List[int]] = {"basket": list(range(42, 50,2)), "africa": list(range(6, 14, 2)),
--- scripts/blender_seq1_vcurf.sh
+++ scripts/blender_seq1_vcurf.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 
 DATASET_PATH=/mnt/Data2/nerf_datasets/nerf_synthetic/
-EXP_PATH=./output/nerf_synthetic_r_0.1
+EXP_PATH=./output/nerf_synthetic_r_0.3
 #EXP_PATH=./output/nerf_synthetic_debug
 
 scenes=(ship chair lego drums hotdog ficus materials mic)
@@ -20,6 +20,6 @@ do
     echo python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=0 --schema v20seq1_inplace --iterations 20000 --run_time ${RUN_TIME}
 
     python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=0 --schema v20seq1_inplace \
-           --iterations 30000 --n_vcam=8 --r_scale=0.1 --run_time ${RUN_TIME} --white_background
+           --iterations 30000 --n_vcam=8 --r_scale=0.3 --run_time ${RUN_TIME} --white_background
 
 done
