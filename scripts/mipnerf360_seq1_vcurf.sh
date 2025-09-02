707b919e788a8ed8888dc3bc9d00953184b0ab7e
lly00412 <315885469@qq.com>
2025-09-02 15:44:22 -0400

    Update mipnerf360_seq1_vcurf.sh

--- scripts/mipnerf360_seq1_vcurf.sh
+++ scripts/mipnerf360_seq1_vcurf.sh
@@ -1,29 +1,52 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=0
 
 DATASET_PATH=/mnt/Data2/nerf_datasets/m360/
-EXP_PATH=./output/m360_data
+EXP_PATH=./output/m360
 
 scenes=(kitchen garden bicycle counter bonsai flowers room stump)
-#scenes=(garden)
 seeds=(0 100 500 1000 1500 2000 2500 3000 3500 4000)
 
-for SEED in ${seeds[@]}
+#for SEED in ${seeds[@]}
+#do
+#  # Define run-time once
+#  RUN_TIME=$(date +%Y%m%d_%H%M%S)
+#
+#  # Optionally create a subfolder with this version
+#  EXP_PATH_WITH_TIME=${EXP_PATH}_${RUN_TIME}
+#
+#  for OBJ in ${scenes[@]}
+#  do
+#      SCENE_PATH=${DATASET_PATH}/${OBJ}
+#      MODEL_PATH=${EXP_PATH_WITH_TIME}/${OBJ}
+#
+#      echo python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=${SEED} --schema v5seq1_inplace --iterations 1000 --run_time ${RUN_TIME}
+#
+##      python train_uncertainty_clf.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=${SEED} --schema v5seq1_inplace \
+##             --iterations 1000 --n_vcam=8 --r_scale=0.3 --run_time ${RUN_TIME}
+#      MLP_CKPT=
+#
+#      python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=mlp --seed=${SEED} --schema v20seq1_inplace \
+#             --iterations 30000 --n_vcam=8 --r_scale=0.3
+#
+#  done
+#done
+
+# Define run-time once
+RUN_TIME=$(date +%Y%m%d_%H%M%S)
+
+# Optionally create a subfolder with this version
+EXP_PATH_WITH_TIME=${EXP_PATH}_${RUN_TIME}
+
+for OBJ in ${scenes[@]}
 do
-  # Define run-time once
-  RUN_TIME=$(date +%Y%m%d_%H%M%S)
+    SCENE_PATH=${DATASET_PATH}/${OBJ}
+    MODEL_PATH=${EXP_PATH_WITH_TIME}/${OBJ}
 
-  # Optionally create a subfolder with this version
-  EXP_PATH_WITH_TIME=${EXP_PATH}_${RUN_TIME}
+    echo python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=${SEED} --schema v5seq1_inplace --iterations 1000 --run_time ${RUN_TIME}
 
-  for OBJ in ${scenes[@]}
-  do
-      SCENE_PATH=${DATASET_PATH}/${OBJ}
-      MODEL_PATH=${EXP_PATH_WITH_TIME}/${OBJ}
+    MLP_CKPT=./ckpts/m360/${OBJ}_psnr/epoch=299.ckpt
 
-      echo python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=${SEED} --schema v5seq1_inplace --iterations 1000 --run_time ${RUN_TIME}
+    python active_train.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=mlp --seed=0 --schema v20seq1_inplace \
+           --iterations 30000 --n_vcam=8 --r_scale=0.3 --mlp_ckpt=${MLP_CKPT}
 
-      python train_uncertainty_clf.py -s ${SCENE_PATH} -m ${MODEL_PATH} --eval --method=vcurf --seed=${SEED} --schema v5seq1_inplace \
-             --iterations 1000 --n_vcam=8 --r_scale=0.3 --run_time ${RUN_TIME}
-
-  done
 done
